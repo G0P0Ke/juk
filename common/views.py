1bc24d0be2f0c1c27b09370941ad7f510aecdd97
Titov Dmitry <dm.a.titov@ya.ru>
2020-03-02 00:27:12 +0300

    add index_page and base.html

--- common/views.py
+++ common/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_page(request):
+
+    context = {
+
+    }
+
+    return render(request, 'pages/index.html', context)
--- juk/urls.py
+++ juk/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import common.views as common_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', common_views.index_page, name='index')
 ]
