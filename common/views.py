65a85c89a6e738ea5eea18a560ef0d2250cbcbe5
Titov Dmitry <dm.a.titov@ya.ru>
2020-03-04 10:23:37 +0300

    add authorization_page

--- common/views.py
+++ common/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
 
-def index_page(request):
-
+def _get_base_context(title):
     context = {
+        'title': title,
 
     }
+    return context
 
+
+def index_view(request):
+    context = _get_base_context('JUK')
     return render(request, 'pages/index.html', context)
+
+
+def authorization_view(request):
+    context = _get_base_context('authorization')
+    if request.method == 'POST':
+        pass
+    context.update({
+        'error': 'Неправильный логин или пароль',
+
+    })
+    return render(request, 'accounts/authorization/authorization_page.html', context)
