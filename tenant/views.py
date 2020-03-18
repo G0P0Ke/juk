3085d6e9b3c4590d0a0d8e747597363413d07195
Daria <dapanfilova@icloud.com>
2020-03-18 19:52:52 +0300

    profile page, edit profile

--- tenant/views.py
+++ tenant/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from tenant.forms import EditProfileForm
+
+
+@login_required
+def profile(request):
+    context = {
+        'user': request.user,
+    }
+    if request.method == 'POST':
+        editprof = EditProfileForm(request.POST, instance=request.user)
+        if editprof.is_valid():
+            editprof.save()
+            u = request.user
+            u.save()
+            context.update({
+                'editprof': editprof,
+            })
+        return redirect('profile')
+    else:
+        editprof = EditProfileForm(instance=request.user)
+
+    context.update({
+        'editprof': editprof,
+    })
+
+    return render(request, 'user_profile/profile.html', context)
+
+
+
+@login_required
+def edit_profile(request):
+    context = {}
+
+    if request.method == 'POST':
+        editprof = EditProfileForm(request.POST, instance=request.user)
+        if editprof.is_valid():
+            editprof.save()
+        return redirect('profile')
+    else:
+        editprof = EditProfileForm(instance=request.user)
+
+    context.update({
+        'editprof' : editprof,
+    })
+
+    return render(request, '', context)
