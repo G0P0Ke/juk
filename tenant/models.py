6da385f28985ad5231b1b8c8bc33d7d5050027be
Petr Babkin <p.k.babkin@gmail.com>
2020-03-21 13:53:36 +0300

    map

--- tenant/models.py
+++ tenant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Company(models.Model):
+    inn = models.CharField(max_length=10)
+
+
+class House(models.Model):
+    address = models.CharField(max_length=50)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Tenant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # name inside
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
