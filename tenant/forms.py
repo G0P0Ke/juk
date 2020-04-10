b21701c6c815d01962e08ae08cd69cc5707c98ee
Petr Babkin <p.k.babkin@gmail.com>
2020-04-11 01:35:35 +0300

    lil

--- tenant/forms.py
+++ tenant/forms.py
@@ -4,6 +4,5 @@ from django import forms
 class DiscussionCreationForm(forms.Form):
     theme = forms.CharField(max_length=20, required=True, label="Тема")
     category = forms.CharField(max_length=20, required=True, label="Категория")
-    description = forms.CharField(max_length=40, required=True, label="Описание")
+    description = forms.CharField(max_length=100, required=True, label="Описание")
     anonymous = forms.BooleanField(required=False)
-
