a6f445baa35b63fc1d1d5969eeacdf5a54027644
Шашкова Екатерина <shashkova.kat@gmail.com>
2020-04-25 17:03:28 +0300

    add forms

--- tenant/forms.py
+++ tenant/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 
+class DiscussionCreationForm(forms.Form):
+    theme = forms.CharField(max_length=20, required=True, label="Тема")
+    category = forms.CharField(max_length=20, required=True, label="Категория")
+    description = forms.CharField(max_length=20, required=True, label="Описание")
+    anonymous = forms.BooleanField(required=False, label="Анонимность")
--- tenant/views.py
+++ tenant/views.py
@@ -14,6 +14,87 @@ def profile_view(request):
     return render(request, 'profile.html', context)
 
 
+@login_required
+def redact_profile_view(request):
+    context = {
+        "house_doesnt_exist": False,
+    }
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        address = request.POST.get('address')
+        request.user.username = username
+        if House.objects.filter(address=address).exists():
+            request.user.tenant.house = House.objects.filter(address=address)[0]
+        else:
+            context.update({
+                "house_doesnt_exist": True,
+                "user": request.user,
+            })
+            return render(request, 'redact_profile.html', context)
+
+        request.user.save()
+        return redirect('/profile')
+    context.update({
+        "user": request.user,
+    })
+    return render(request, 'redact_profile.html', context)
+
+
+class Category:
+    """
+    Служебный класс для передачи данных в context
+    """
+    def __init__(self, name, list_of_discussions):
+        """
+        :param name: название категории
+        :param list_of_discussions: список обсуждений, относящийся к нему
+        """
+        self.name = name
+        self.list_of_discussions = list_of_discussions
+
+def helpdesk_view(request, id):
+    context = {}
+    helpdesk = HelpDesk.objects.get(pk=id)
+    if request.method == 'POST':
+        text = request.POST.get('message')
+        # проверка на жителя
+        message = HelpDeskSmartMessage.objects.create(text=text, helpdesk=helpdesk,
+                                         creator="user", cr_date=datetime.now())
+        # проверка на УК
+        # message = Message.objects.create(text=text, helpdesk=helpdesk,
+        #                                          creator="company", cr_date=datetime.now())
+        message.save()
+    messages = helpdesk.helpdesksmartmessage_set.all()
+    messages = list(messages)
+    messages.reverse()
+    context.update({
+        "user": request.user,
+        "helpdesk": helpdesk,
+        "smartmessages": messages,
+    })
+    return render(request, 'helpdesk.html', context)
+
+
+def cr_helpdesk_view(request):
+    context = {}
+    if request.method == 'POST':
+        theme = request.POST.get('theme')
+        inn = int(request.POST.get("inn"))
+        company = Company.objects.filter(inn=inn)[0]
+        if request.user.tenant:
+            helpdesk = HelpDesk(theme=theme, company=company,
+                                user=request.user, cr_date=datetime.now())
+            helpdesk.save()
+        # if проверка на то что это представитель компании
+        return redirect('/helpdesk/' + str(helpdesk.id))
+    context.update({
+        "user": request.user,
+        "companies": Company.objects.all(),
+        "is_tenant": True if request.user.tenant else False #-----------проверка на то является ли жителем------
+    })
+    return render(request, 'cr_helpdesk.html', context)
+
+
 class Category:
     """
     Служебный класс для передачи данных в context
