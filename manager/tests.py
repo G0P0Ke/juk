40f220504fc410e4865c8948fd73ba6453ae05a9
marcheanin <marcheanin1576@gmail.com>
2020-05-30 15:49:31 +0300

    add some tests

--- manager/tests.py
+++ manager/tests.py
@@ -1,12 +1,31 @@
 import unittest
+
+from django.contrib.auth import authenticate
 from django.test import Client
 
 
 class SimpleTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
+        self.user = get_user_model().objects.create_user(username='test', password='12test12', email='test@example.com')
+        self.user.save()
 
     def test_details(self):
         response = self.client.get('/customer/datails/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.conext['customers']), 5)
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username='test', password='12test12')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='wrong', password='12test12')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_pssword(self):
+        user = authenticate(username='test', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
