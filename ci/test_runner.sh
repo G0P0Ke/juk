617141cb86af43cb61a92b64e7e7f5bb0402da51
Feodor <goncharov.feodor@mail.ru>
2020-06-03 13:56:49 +0300

    tenant test added to ci

--- ci/test_runner.sh
+++ ci/test_runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir public
-python3 manage.py test manager common | tee public/test.txt
-coverage run manage.py test manager common 
+python3 manage.py test tenant  manager common | tee public/test.txt
+coverage run manage.py test tenant manager common
 coverage report | tee public/code_coverage.txt
 exit 0
