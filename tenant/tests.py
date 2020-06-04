dc0f5ff677d1d888cc4eecbef21800df0bbd0cab
Shabalkin Vitaliy <shabalkin_vv@mail.ru>
2020-06-04 05:41:01 +0300

    Some tests by SV

---
+++ tenant/tests.py
@@ -0,0 +1,90 @@
+import unittest
+from django.test import Client
+
+
+class SimpleTest(unittest.TestCase):
+
+    def test_cr_appeal(self):
+     """
+     Тест страницы
+     """
+     self.client.login(username='tenant-test', password='promprog')
+     response = self.client.get('/cr_appeal/')
+     self.assertEqual(response.status_code, 200)
+     self.client.login(username='manager-test', password='promprog')
+     response = self.client.get('/cr_appeal/')
+     self.assertEqual(response.status_code, 200)
+
+    def test_vol_test(self):
+     """
+     Тест страницы
+     """
+     self.client.login(username='tenant-test', password='promprog')
+     response = self.client.get('/vol/test/')
+     self.assertEqual(response.status_code, 200)
+
+     self.client.login(username='manager-test', password='promprog')
+     response = self.client.get('/vol/test/')
+     self.assertEqual(response.status_code, 302)
+     self.assertRedirects(response, '/')
+
+    def test_volunteer(self):
+     """
+     Тест страницы
+     """
+     self.client.login(username='tenant-test', password='promprog')
+     response = self.client.get('/vol/volunteer/')
+     self.assertEqual(response.status_code, 200)
+     self.client.login(username='manager-test', password='promprog')
+     response = self.client.get('/vol/volunteer/')
+     self.assertEqual(response.status_code, 200)
+
+    def test_help(self):
+         """
+         Тест страницы
+         """
+         self.client.login(username='tenant-test', password='promprog')
+         response = self.client.get('/vol/help/')
+         self.assertEqual(response.status_code, 200)
+         self.client.login(username='manager-test', password='promprog')
+         response = self.client.get('/vol/help/')
+         self.assertEqual(response.status_code, 200)
+
+    def test_help_cr_task(self):
+     """
+     Тест страницы
+     """
+     self.client.login(username='tenant-test', password='promprog')
+     response = self.client.get('/vol/help/cr_task/')
+     self.assertEqual(response.status_code, 200)
+     self.client.login(username='manager-test', password='promprog')
+     response = self.client.get('/vol/help/cr_task/')
+     self.assertEqual(response.status_code, 200)
+
+    def test_my_passes(self):
+         """
+         Тест страницы
+         """
+         self.client.login(username='tenant-test', password='promprog')
+         response = self.client.get('/tenant/pass/my_passes/')
+         self.assertEqual(response.status_code, 200)
+
+         self.client.login(username='manager-test', password='promprog')
+         response = self.client.get('/tenant/pass/my_passes/')
+         self.assertEqual(response.status_code, 302)
+         self.assertRedirects(response, '/')
+
+    def test_cr_pass(self):
+        """
+        Тест страницы
+        """
+        self.client.login(username='tenant-test', password='promprog')
+        response = self.client.get('/tenant/pass/cr_pass/')
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='manager-test', password='promprog')
+        response = self.client.get('/tenant/pass/cr_pass/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+
+
